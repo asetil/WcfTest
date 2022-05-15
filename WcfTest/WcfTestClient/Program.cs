2e1f1b91ed02f34f9547e76a759f4ad8ff8ec161
asetil <osman.sokuoglu@gmail.com>
2022-05-15 18:14:51 +0300

    test client added

---
+++ WcfTest/WcfTestClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WcfTestClient.Calculator;
+
+namespace WcfTestClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorServiceClient client = new CalculatorServiceClient();
+            var response = client.Calculate(new CalculateItem()
+            {
+                Operation = "Sum",
+                ValueList = (new List<int>() { 4, 5, 3, 8 }).ToArray(),
+            });
+        }
+    }
+}
